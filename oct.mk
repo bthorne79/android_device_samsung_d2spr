fd324cd34452b6e7349e44bab3d00ef7f559e3be
bthorne79 <rcovington79@gmail.com>
2014-01-23 14:48:27 -0500

    Update and rename cm.mk to oct.mk

--- oct.mk
+++ oct.mk
@@ -1,10 +1,5 @@
 $(call inherit-product, device/samsung/d2spr/full_d2spr.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2spr \
@@ -12,6 +7,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="samsung/d2spr/d2spr:4.1.2/JZO54K/L710VPBMD4:user/release-keys" \
     PRIVATE_BUILD_DESC="d2spr-user 4.1.2 JZO54K L710VPBMD4 release-keys"
 
-PRODUCT_NAME := cm_d2spr
+PRODUCT_NAME := oct_d2spr
 PRODUCT_DEVICE := d2spr
 
