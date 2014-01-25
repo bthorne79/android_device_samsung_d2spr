5a71f53f69f784ceef689252718ff13ebdaef03e
bthorne79 <rcovington79@gmail.com>
2014-01-25 02:07:55 -0500

    Update oct.mk

--- oct.mk
+++ oct.mk
@@ -1,5 +1,11 @@
 $(call inherit-product, device/samsung/d2spr/full_d2spr.mk)
 
+# Enhanced NFC
+$(call inherit-product, vendor/oct/config/nfc_enhanced.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/oct/config/common_full_phone.mk)
+
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2spr \
